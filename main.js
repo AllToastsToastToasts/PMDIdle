30b6b1a0a4bd236035c58df98f9670462b3f8da2
AllToastsToastToasts <ddddd212@tlen.pl>
2016-03-19 23:39:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,16 +5,12 @@ var firstDig = 0;
 function refreshStats(){
 	document.getElementById("HP").innerHTML = HP;
 	document.getElementById("firstDig").innerHTML = firstDig;
-	document.getElementbyId("firstDigtext").innerHTML = firstDigtext;
 }
 
 function dig(){
 	HP = HP - 1
 	firstDig = firstDig + 1
 	refreshStats();
-        if(firstDig >= 95){
-	firstDigtext = You started to dig...
-        }
 }
 
 function rest(){
