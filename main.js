37c55e4f16fb9ea2f4ede12139c6d01db996bcf0
AllToastsToastToasts <ddddd212@tlen.pl>
2016-03-20 13:11:36 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
 
-var health = 100;
-var firstDig = 0;
-var firstDigtext = 'You are somewhere...';
+var health = 100
+var firstDig = 0
+var firstDigtext = 'You are somewhere...'
 var soil = 0;
 
 function refreshStats(){
-	document.getElementById("health").innerHTML = health;
-	document.getElementById("firstDig").innerHTML = firstDig;
-        document.getElementById("firstDigtext").innerHTML = firstDigtext;
+	document.getElementById("health").innerHTML = health
+	document.getElementById("firstDig").innerHTML = firstDig
+        document.getElementById("firstDigtext").innerHTML = firstDigtext
         document.getElementById("soil").innerHTML = soil;
 }
 
 function dig(){
-	health -= 1;
-	soil += 1;
-	firstDig += 1;
+	health -= 1
+	soil += 1
+	firstDig += 1
 	        if(firstDig >= 1){
-	firstDigtext = 'You started to dig...';
+	firstDigtext = 'You started to dig...'
 	        	}
 	refreshStats();
 }
 
 function rest(){
-	health += 1;
+	health += 1
 	refreshStats();
 }
 
