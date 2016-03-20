7633ea264c69c8014e2fa4ffc221e71dfa238726
AllToastsToastToasts <ddddd212@tlen.pl>
2016-03-20 13:36:14 +0100

    Update main.js

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ function refreshStats(){
 
 function dig(){
 	health -= 1
-	soil += math.random(0.1, 0.5)
+	soil += 0.1
 	firstDig += 1
 	        if(firstDig >= 1){
 	firstDigtext = 'You started to dig...'
